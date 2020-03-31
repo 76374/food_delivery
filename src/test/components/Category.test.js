72a362841c1dc27822bb46128ca32d24e2056080
Alex Bolbat <alexandr.bolbat@sigma.software>
2020-03-31 17:55:39 +0300

    order page tests

---
+++ src/test/components/Category.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import Category from "../../components/Category/Category";
+import MealCard from "../../components/Category/MealCard/MealCard";
+import { getMenuData } from '../mockState';
+
+describe('<Category/>', () => {
+    const menuData = getMenuData(5);
+    const catData = menuData[0];
+    const wrapper = shallow(
+        <Category
+            items={catData.items}
+            title={catData.title}
+            orderedItems={[]}
+        />
+    );
+
+    it('should contain correct number of the items', () => {
+        expect(wrapper.find(MealCard)).toHaveLength(5);
+    });
+
+    it('should have correct title', () => {
+        expect(wrapper.find('h2').text()).toBe(menuData[0].title)
+    });
+});
--- src/test/containers/Order.test.js
+++ src/test/containers/Order.test.js
@@ -1,6 +1,28 @@
+import React from 'react';
+import { getMenuData } from "../mockState";
+import { Provider } from "react-redux";
+import Order from "../../containers/Order/Order";
+import { createStore } from 'redux';
+import reducer from '../../store/reducers';
+import { mount } from 'enzyme';
+import Category from '../../components/Category/Category';
+
 describe('<Order />', () => {
-    //const state
-    it ('_', () => {
-        expect(true).toBe(true);
+    const menuData = getMenuData(1, 2, 3);
+    const state = {
+        order: { 
+            menuData,
+            orderedItems: []
+        }
+    };
+    const getWrapper = (mockStore = createStore(reducer, state)) => mount(
+        <Provider store={mockStore}>
+            <Order/>
+        </Provider>
+    );
+
+    it ('should contain correct number of categories', () => {
+        const wrapper = getWrapper();
+        expect(wrapper.find(Category)).toHaveLength(3);
     });
 });
