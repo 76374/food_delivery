2a2ac97faa09cec1bfc396a34a15c7988285b51e
Alex Bolbat <alexandr.bolbat@sigma.software>
2020-04-09 21:31:15 +0300

    removed redux strore

--- src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import order from './order';
-import appState from './appState';
-
-const reducer = combineReducers({ order, appState });
-
-export default reducer;
--- src/store/utils/index.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const combineObjects = (obj1, obj2) => (
-  { ...obj1, ...obj2 }
-);
