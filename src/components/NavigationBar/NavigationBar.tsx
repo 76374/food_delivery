e8e0d4b8970a73176cd53b66deb4105105160919
Alex Bolbat <alexandr.bolbat@sigma.software>
2020-06-19 00:34:42 +0300

    added bootstrap; changed navbar; added a background

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavigationItem from './NavigationItem/NavigationItem';
 import styles from './NavigationBar.module.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/NavBar';
 
 export interface NavigationItemData {
   text: string;
@@ -13,11 +16,18 @@ interface NavigationBarProps {
 
 const NavigationBar = (props: NavigationBarProps) => {
   let key = 0;
-  const navigationItems = props.items.map((item) => (
-    <NavigationItem text={item.text} link={item.link} key={`#navBarItem${key++}`} />
+  const navigationItems = props.items.map((item, index) => (
+    //<NavigationItem text={item.text} link={item.link} key={`#navBarItem${key++}`} />
+    <Nav.Link as={Link} to={item.link} eventKey={index} key={`#navBarItem${key++}`}>
+      {item.text}
+    </Nav.Link>
   ));
 
-  return <div className={styles.NavigationBar}>{navigationItems}</div>;
+  return (
+    <Navbar bg="light" expand="sm">
+      <Nav>{navigationItems}</Nav>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
--- src/containers/TopBar/TopBar.tsx
+++ src/containers/TopBar/TopBar.tsx
@@ -1,35 +1,38 @@
 import React from 'react';
-import NavigationBar, { NavigationItemData } from '../../components/NavigationBar/NavigationBar';
-import styles from './TopBar.module.css';
+import { Link, useLocation } from 'react-router-dom';
+import { observer } from 'mobx-react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/NavBar';
 import AppPath from '../../const/AppPath';
 import Locale from '../../service/Locale';
 import LocaleKey from '../../const/LocaleKey';
 import useStore from '../../hooks/useStore';
-import { observer } from 'mobx-react';
 
-const getItemData = (link: string, text: string) => ({ link, text });
+const getLink = (link: string, text: string) => (
+  <Nav.Link as={Link} to={link} eventKey={link} key={`#navBarItem${link}`}>
+    {text}
+  </Nav.Link>
+);
 
 const TopBar = () => {
   const { user } = useStore();
-  const navigationItems: NavigationItemData[] = [
-    getItemData(AppPath.ORDER, Locale.get(LocaleKey.TOP_BAR_BT_ORDER)),
-    getItemData(AppPath.CHECKOUT, Locale.get(LocaleKey.TOP_BAR_BT_CHECKOUT)),
-  ];
-  if (user.isSignedIn) {
-    navigationItems.push(
-      getItemData(AppPath.LOGOUT, Locale.get(LocaleKey.TOP_BAR_BT_LOGOUT, String(user.firstName)))
-    );
-  } else {
-    navigationItems.push(
-      getItemData(AppPath.SIGN_IN, Locale.get(LocaleKey.TOP_BAR_BT_SIGN_IN)),
-      getItemData(AppPath.SIGN_UP, Locale.get(LocaleKey.TOP_BAR_BT_SIGN_UP))
-    );
-  }
+  const location = useLocation();
+
+  const signControls = user.isSignedIn
+    ? getLink(AppPath.LOGOUT, Locale.get(LocaleKey.TOP_BAR_BT_LOGOUT, String(user.firstName)))
+    : [
+        getLink(AppPath.SIGN_IN, Locale.get(LocaleKey.TOP_BAR_BT_SIGN_IN)),
+        getLink(AppPath.SIGN_UP, Locale.get(LocaleKey.TOP_BAR_BT_SIGN_UP)),
+      ];
 
   return (
-    <div className={styles.TopBar}>
-      <NavigationBar items={navigationItems} />
-    </div>
+    <Navbar expand="sm">
+      <Nav activeKey={location.pathname}>
+        {getLink(AppPath.ORDER, Locale.get(LocaleKey.TOP_BAR_BT_ORDER))}
+        {getLink(AppPath.CHECKOUT, Locale.get(LocaleKey.TOP_BAR_BT_CHECKOUT))}
+        {signControls}
+      </Nav>
+    </Navbar>
   );
 };
 
